2d3c5525fe7a096da81c50fb8a09fe59097d67ad
Daniel Maly <maly.dan@gmail.com>
2015-04-19 18:52:13 +0200

    Roadmap for png encoding

---
+++ brainx/png_encoder.py
@@ -0,0 +1,12 @@
+__author__ = 'Daniel Maly'
+
+# Common PNG writer with pixels already ready
+
+# Grid generator for given width, height and program
+
+# Pixel generator for brainloller given a grid
+
+# Pixel generator for braincopter given the source image and the grid
+
+# Brainloller encoder: Program -> optimal width and height -> grid -> pixels -> write PNG
+# Braincopter encoder: Program, source image -> grid -> source image + grid -> pixels -> write PNG
